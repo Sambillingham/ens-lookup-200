79f1121bae966b411895ebfbb8d4703239441ef1
Sambillingham <hello@sambillingham.com>
2023-01-19 13:53:33 +0100

    add: basic styles + loading skeleton

--- components/EnsFeed.js
+++ components/EnsFeed.js
@@ -1,5 +1,9 @@
 import {gql, useQuery} from "@apollo/client";
 import { useState } from 'react';
+import Skeleton from 'react-loading-skeleton'
+
+import styles from '@/styles/EnsFeed.module.css'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const GET_REG = gql`
 query GetDomains($limit: Int!, $skip: Int!) {
@@ -22,31 +26,49 @@ query GetDomains($limit: Int!, $skip: Int!) {
 function GetENS() {
     const [page, setPage] = useState(0);
 
-    const { loading, error, data, startPolling } = useQuery(GET_REG, { 
+    let { loading, error, data, startPolling } = useQuery(GET_REG, { 
     variables: {
         limit: 10,
         skip: page * 10,
     }
     });
-    startPolling(500)
+    startPolling(5000)
 
-    if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
 
     console.log(data)
-    return (
-    <>
-    page: {page}
-    <div>
+
+    if (loading) return (    <div>
+        <h2>Live Data Feed</h2>
         {
-            data.registrations.map(({ domain }) => (
-            <ul key={domain.name} >
-                <li>Name: {domain.name}</li>
+            [...Array(5)].map((x, i) => (
+            <ul key={i} className={styles.feedItem}>
+                <Skeleton count={5} />
             </ul>
             ))
         }
+    </div> )
+
+    return (
+    <>
+    <div>
+        <h2>Live Data Feed</h2>
+        
+        {
+            data.registrations.map(({ domain }) => (
+            <ul key={domain.name} className={styles.feedItem}>
+                <li>Name: {domain.name}</li>
+                <li>Registrant: {data.registrations[0].registrant?.id} </li>
+                <li>Assigned Addr: {data.registrations[0].domain.resolvedAddress?.id}</li>
+                <li>Registration: {data.registrations[0].registrationDate}</li>
+                <li>Expiry: {data.registrations[0].expiryDate}</li>
+            </ul>
+            ))
+        }
+
     </div>
     <div>
+        page: {page}
         <button onClick={() => setPage(prev => prev - 1)}>Prev</button>
         <button onClick={() => setPage(prev => prev + 1)}>Next</button>
     </div>
